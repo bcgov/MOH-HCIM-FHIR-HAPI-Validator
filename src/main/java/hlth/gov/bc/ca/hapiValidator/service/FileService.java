b324c2a485d6cf649767a2cb674232d78d2488b4
Joshua Burton <joshua.burton@cgi.com>
2026-02-13 14:22:09 -0800

    HCIM-600: First commit into the GitHub repo for the HAPI Validator tool. Committing currently done work for the tool; has yet to be run with test data

---
+++ src/main/java/hlth/gov/bc/ca/hapiValidator/service/FileService.java
@@ -0,0 +1,66 @@
+package hlth.gov.bc.ca.hapiValidator.service;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import org.apache.commons.io.IOUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Value("${spring.hapi.input.path}")
+    private String inputPath;
+
+    @Value("${spring.hapi.output.path}")
+    private String outputPath;
+
+    /**
+     * Reads the content of an input file containing a request/response message and returns it as a string.
+     * 
+     * @param fileName The input file name.
+     * @return The content of the input file as a string.
+     */
+    public String readMessage(String fileName) {
+        try (InputStream inputStream = resourceLoader.getResource("file:" + inputPath + "/" + fileName).getInputStream()) {
+            return (IOUtils.toString(inputStream, "UTF-8"));
+            
+        } catch (NullPointerException | IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Writes the validation results for an input file into a correpsonding output text file.
+     * 
+     * @param fileName The output file name.
+     * @param inputFileName The name of the input file being validated to write as a header for these results.
+     * @param validationResults The results to write into the output file.
+     */
+    public void writeResults(String fileName, String inputFileName, List<String> validationResults) {
+        try {
+            Path path = Paths.get(outputPath + "/" + fileName);
+            Files.writeString(path, "\n" + inputFileName + ":\n", StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            Files.write(path, validationResults, StandardOpenOption.APPEND);
+            Files.writeString(path, "-------------------------\n", StandardOpenOption.APPEND);
+
+        } catch (InvalidPathException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
