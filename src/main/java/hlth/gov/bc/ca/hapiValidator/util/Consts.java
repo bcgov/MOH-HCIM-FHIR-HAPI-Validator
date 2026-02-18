893d3aef44f795dc7e1251da5ee4d901507329a3
Joshua Burton <joshua.burton@cgi.com>
2026-02-17 16:40:06 -0800

    HCIM-600: Improving output file format and code maintainability, and fixing HAPI FHIR Cache packaging issues which was causing the validator to fail. This version has had the first round of testing and has successfully done validations with the baseline HAPI definitions against HCIM FHIR messages (RP in and out messages). Next step is to determine if/how to implement our Terminology service's custom definitions.

--- src/main/java/hlth/gov/bc/ca/hapiValidator/util/Consts.java
+++ src/main/java/hlth/gov/bc/ca/hapiValidator/util/Consts.java
@@ -1,9 +1,12 @@
 package hlth.gov.bc.ca.hapiValidator.util;
 
+import java.text.SimpleDateFormat;
+
 /**
  *
  * @author CGI
  */
 public class Consts {
-    
+    public static final String LINE_SEPARATOR = "------------------------------------------------\n";
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmmss");
 }
